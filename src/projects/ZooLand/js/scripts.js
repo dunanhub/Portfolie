487d8716e81bd30304a6780730533bdc89931007
Beka <104020748+dunanhub@users.noreply.github.com>
2024-10-15 23:36:28 +0200

    Add files via upload

---
+++ src/projects/ZooLand/js/scripts.js
@@ -0,0 +1,49 @@
+let loginForm = document.querySelector('.header .login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');    
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "больше";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "меньше";
+      moreText.style.display = "inline";
+    }
+  }
